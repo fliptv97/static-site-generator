4b3d24416e98ec6f0431c7d1dcc22ef95089eec5
Philip Kazakov <fliptv97@gmail.com>
2025-05-08 00:23:04 +0300

    misc: github pages support

--- src/generate_content.py
+++ src/generate_content.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from markdown_blocks import markdown_to_html_node
 
 
-def generate_pages(src, dst, template_path):
+def generate_pages(src, dst, template_path, basepath):
     if not os.path.exists(dst):
         os.mkdir(dst)
 
@@ -16,10 +16,11 @@ def generate_pages(src, dst, template_path):
             generate_page(
                 os.path.join(src_path),
                 os.path.join(Path(dst_path).with_suffix(".html")),
-                template_path
+                template_path,
+                basepath
             )
         else:
-            generate_pages(src_path, dst_path, template_path)
+            generate_pages(src_path, dst_path, template_path, basepath)
 
 
 def extract_title(markdown):
@@ -32,7 +33,7 @@ def extract_title(markdown):
     raise ValueError("markdown must have at least one level 1 header")
 
 
-def generate_page(src, dst, template_path):
+def generate_page(src, dst, template_path, basepath):
     print(f"Generating page from {src} to {dst} using {template_path}")
 
     with open(src, encoding="utf-8") as markdown_file:
@@ -42,7 +43,10 @@ def generate_page(src, dst, template_path):
             node = markdown_to_html_node(markdown)
             content = node.to_html()
             template = template_file.read()
-            page = template.replace("{{ Title }}", title).replace("{{ Content }}", content)
+            page = template.replace("{{ Title }}", title)
+            page = page.replace("{{ Content }}", content)
+            page = page.replace("href=\"/", f"href=\"{basepath}")
+            page = page.replace("src=\"/", f"src=\"{basepath}")
 
             os.makedirs(os.path.dirname(dst), exist_ok=True)
 
--- src/main.py
+++ src/main.py
@@ -1,25 +1,33 @@
 import os
 import shutil
+import sys
 
 from copy_dir_content import copy_dir_content
 from generate_content import generate_pages
 
 
-DIR_PATH_PUBLIC = "./public"
+DIR_PATH_DOCS = "./docs"
 DIR_PATH_STATIC = "./static"
 DIR_PATH_CONTENT = "./content"
 TEMPLATE_PATH = "./template.html"
 
 
 def main():
+    basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
+
     print("Deleting public directory...")
-    if os.path.exists(DIR_PATH_PUBLIC):
-        shutil.rmtree(DIR_PATH_PUBLIC)
+    if os.path.exists(DIR_PATH_DOCS):
+        shutil.rmtree(DIR_PATH_DOCS)
 
     print("Copying static files to public directory...")
-    copy_dir_content(DIR_PATH_STATIC, DIR_PATH_PUBLIC)
+    copy_dir_content(DIR_PATH_STATIC, DIR_PATH_DOCS)
 
-    generate_pages(DIR_PATH_CONTENT, DIR_PATH_PUBLIC, TEMPLATE_PATH)
+    generate_pages(
+        src=DIR_PATH_CONTENT,
+        dst=DIR_PATH_DOCS,
+        template_path=TEMPLATE_PATH,
+        basepath=basepath
+    )
 
 
 if __name__ == "__main__":
