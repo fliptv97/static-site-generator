fd6c206966d6d2cef5bf9682c0985102b13203cb
Philip Kazakov <fliptv97@gmail.com>
2025-05-07 15:55:13 +0300

    feat: helper to convert TextNode to LeafNode

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -29,6 +29,13 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("Placeholder text", TextType.PLAIN)
 
         self.assertEqual(repr(node), "TextNode(Placeholder text, plain, None)")
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.PLAIN)
+        html_node = text_node_to_html_node(node)
+
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
 
 
 if __name__ == "__main__":
