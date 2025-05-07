616faa3be8a39e78da1f4a71f35ffcbd2f35b562
Philip Kazakov <fliptv97@gmail.com>
2025-05-07 18:08:27 +0300

    feat: add helpers to split image and link nodes

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -32,5 +32,73 @@ def extract_markdown_images(text):
     return re.findall(r"\!\[(.*?)\]\((.*?)\)", text)
 
 
+def split_nodes_image(old_nodes):
+    new_nodes = []
+
+    for node in old_nodes:
+        if node.text_type != TextType.PLAIN:
+            new_nodes.append(node)
+            continue
+
+        text_so_far = node.text
+        images = extract_markdown_images(text_so_far)
+
+        if len(images) == 0:
+            new_nodes.append(node)
+            continue
+
+        for (alt_text, url) in images:
+            sections = text_so_far.split(f"![{alt_text}]({url})", 1)
+
+            if len(sections) != 2:
+                raise ValueError("invalid markdown, image section isn't closed")
+
+            text_so_far = sections[1]
+
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.PLAIN))
+
+            new_nodes.append(TextNode(alt_text, TextType.IMAGE, url))
+
+        if text_so_far != "":
+            new_nodes.append(TextNode(text_so_far, TextType.PLAIN))
+
+    return new_nodes
+
+
 def extract_markdown_links(text):
     return re.findall(r"(?<!!)\[(.*?)\]\((.*?)\)", text)
+
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+
+    for node in old_nodes:
+        if node.text_type != TextType.PLAIN:
+            new_nodes.append(node)
+            continue
+
+        text_so_far = node.text
+        links = extract_markdown_links(text_so_far)
+
+        if len(links) == 0:
+            new_nodes.append(node)
+            continue
+
+        for (alt_text, url) in links:
+            sections = text_so_far.split(f"[{alt_text}]({url})", 1)
+
+            if len(sections) != 2:
+                raise ValueError("invalid markdown, link section isn't closed")
+
+            text_so_far = sections[1]
+
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.PLAIN))
+
+            new_nodes.append(TextNode(alt_text, TextType.LINK, url))
+
+        if text_so_far != "":
+            new_nodes.append(TextNode(text_so_far, TextType.PLAIN))
+
+    return new_nodes
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,7 +1,13 @@
 import unittest
 
 from textnode import TextNode, TextType
-from inline_markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
+from inline_markdown import (
+    split_nodes_delimiter,
+    split_nodes_image,
+    split_nodes_link,
+    extract_markdown_images,
+    extract_markdown_links
+)
 
 
 class TestInlineMarkdown(unittest.TestCase):
@@ -26,6 +32,42 @@ class TestInlineMarkdown(unittest.TestCase):
         matches = extract_markdown_links("This is text with a [link text](https://google.com) and an ![image](https://placehold.co/600x400)")
 
         self.assertListEqual(matches, [("link text", "https://google.com")])
+
+
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.PLAIN,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.PLAIN),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.PLAIN),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
+
+
+    def test_split_links(self):
+        node = TextNode(
+            "This is text with an [link](https://google.com) and another [link](https://gmail.com)",
+            TextType.PLAIN,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.PLAIN),
+                TextNode("link", TextType.LINK, "https://google.com"),
+                TextNode(" and another ", TextType.PLAIN),
+                TextNode("link", TextType.LINK, "https://gmail.com"),
+            ],
+            new_nodes,
+        )
 
 
 if __name__ == "__main__":
