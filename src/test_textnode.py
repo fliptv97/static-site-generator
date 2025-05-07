30b83497559c71424601bcbd56686c490a6c2299
Philip Kazakov <fliptv97@gmail.com>
2025-05-07 14:27:21 +0300

    feat: add tests for textnode.py

---
+++ src/test_textnode.py
@@ -0,0 +1,35 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+
+        self.assertEqual(node, node2)
+
+        url = "http://localhost:8888/placeholder.png"
+        image_node = TextNode("This is an image node", TextType.IMAGE, url)
+        image_node2 = TextNode("This is an image node", TextType.IMAGE, url)
+
+        self.assertEqual(image_node, image_node2)
+
+        link_node = TextNode("This is a link node", TextType.LINK, url)
+
+        self.assertNotEqual(image_node, link_node)
+
+    def test_default_url(self):
+        node = TextNode("Image node example", TextType.LINK)
+
+        self.assertEqual(node.url, None)
+
+    def test_repr(self):
+        node = TextNode("Placeholder text", TextType.PLAIN)
+
+        self.assertEqual(repr(node), "TextNode(Placeholder text, plain, None)")
+
+
+if __name__ == "__main__":
+    unittest.main()
