cce0fa5e3cfd2359f275207a292d89232b931d14
Philip Kazakov <fliptv97@gmail.com>
2025-05-07 18:57:42 +0300

    feat: add helper to get block type of block

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,6 +1,47 @@
+from enum import Enum
+import re
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     mapped = map(lambda str: str.strip(), blocks)
     filtered = filter(lambda str: str != "", mapped)
 
     return list(filtered)
+
+
+def block_to_block_type(block):
+    if re.match(r"#{1,6}\s", block) is not None:
+        return BlockType.HEADING
+    if block.startswith("```") and block.endswith("```"):
+        return BlockType.CODE
+
+    lines = block.splitlines()
+    if all(line.startswith(">") for line in lines):
+        return BlockType.QUOTE
+    if all(line.startswith("- ") for line in lines):
+        return BlockType.UNORDERED_LIST
+    if is_ordered_list(lines):
+        return BlockType.ORDERED_LIST
+
+    return BlockType.PARAGRAPH
+
+
+def is_ordered_list(lines):
+    counter = 1
+
+    for line in lines:
+        if not line.startswith(f"{counter}. "):
+            return False
+        counter += 1
+
+    return True
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,6 +1,6 @@
 import unittest
 
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import markdown_to_blocks, block_to_block_type, BlockType
 
 
 class TestBlockMarkdown(unittest.TestCase):
@@ -23,6 +23,63 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+
+    def test_block_to_block_type_headings(self):
+        self.assertEqual(block_to_block_type("# Heading-1"), BlockType.HEADING)
+        self.assertEqual(block_to_block_type("## Heading-2"), BlockType.HEADING)
+        self.assertEqual(block_to_block_type("### Heading-3"), BlockType.HEADING)
+        self.assertEqual(block_to_block_type("#### Heading-4"), BlockType.HEADING)
+        self.assertEqual(block_to_block_type("##### Heading-5"), BlockType.HEADING)
+        self.assertEqual(block_to_block_type("###### Heading-6"), BlockType.HEADING)
+
+
+    def test_block_to_block_type_code(self):
+        md = """
+```
+print("hello world")
+```
+"""
+
+        self.assertEqual(block_to_block_type(md.strip()), BlockType.CODE)
+
+
+    def test_block_to_block_type_quote(self):
+        md = """
+> best
+> quote
+> ever
+"""
+
+        self.assertEqual(block_to_block_type(md.strip()), BlockType.QUOTE)
+
+
+    def test_block_to_block_type_unordered_list(self):
+        md = """
+- first
+- second
+- third
+"""
+
+        self.assertEqual(block_to_block_type(md.strip()), BlockType.UNORDERED_LIST)
+
+
+    def test_block_to_block_type_ordered_list(self):
+        md = """
+1. First
+2. Second
+3. Third
+"""
+
+        self.assertEqual(block_to_block_type(md.strip()), BlockType.ORDERED_LIST)
+
+        invalid_md = """
+2. Milk
+33. Sugar
+42. Life
+"""
+
+        self.assertNotEqual(block_to_block_type(invalid_md.strip()), BlockType.ORDERED_LIST)
 
 
 if __name__ == "__main__":
